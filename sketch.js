3aeba1ccc2b6a274ec8322409e9344acacf6d534
[Ingrid y Angel] <[a01338842@itesm.mx]>
2021-01-08 09:48:23 -0600

    Modificacion del juego de la vida

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let tablero;
 let columnas;
 let renglones;
-let celda_tamanio= 10;
+let celda_tamanio= 5;
+let contador= 0;
 
 function setup() {
   createCanvas(600, 400);
@@ -14,17 +15,36 @@ function setup() {
       print(tablero[x][y]);
     }
   }
-  
+
   //print(tablero);
-  
+
 }
 
 function draw() {
-  background(220);
-  pintatablero();
+  background(300);
+  pintaTablero();
   siguienteGeneracion();
-  
-  
+
+  if (mouseIsPressed){
+    contador= 0
+    text("rebooting...",100, 100);
+    tablero = creaTablero(columnas, renglones);
+  for(let x = 1; x < columnas-1; x+= 1){
+    for(let y = 1; y < renglones-1; y+= 1){
+      tablero[x][y] = floor(random(2));
+      //print(tablero[x][y]);
+    }
+  }
+  }else{
+    pintaTablero(columnas, renglones);
+  siguienteGeneracion();
+
+}
+
+ contador= contador + 1
+  text(contador, 200,100);
+  textSize(30);
+
 }
 
 function siguienteGeneracion(){
@@ -33,7 +53,7 @@ function siguienteGeneracion(){
     for(let y = 1; y < renglones-1; y+= 1){
       let celda = tablero[x][y];
       let vecinos = cuentaVecinos(x,y);
-        
+
       if(celda == 0 && vecinos == 3){
         tablero_siguiente[x][y] = 1;
       }else if(celda == 1 &&(vecinos > 3 || vecinos < 2)){
@@ -41,7 +61,7 @@ function siguienteGeneracion(){
       }else{
         tablero_siguiente[x][y] = celda;
       }
-        
+
     }
   }
 
@@ -60,20 +80,33 @@ function cuentaVecinos(x,y){
   suma_vecinos += tablero[x ][y+1];
   suma_vecinos += tablero[x +1][y+1];
   return suma_vecinos;
-  
+
 }
 
-function pintatablero(cols,ren){
-  for(let x = 0; x < columnas; x+= 1){
-    for(let y = 0; y < renglones; y+= 1){
+function pintaTablero(cols,ren){
+  for(let x = 1; x < columnas-1; x+= 1){
+    for(let y = 1; y < renglones-1; y+= 1){
       let posx = x * celda_tamanio;
       let posy = y * celda_tamanio;
-      if(tablero[x][y] == 1){
-        fill(100, 0, 215);
+      let vecinos = cuentaVecinos(x, y);
+      let celda = tablero[x][y];
+
+      if(celda == 0 && vecinos == 3){
+        fill(370, 2, 0);
         stroke(0);
-        rect(posx, posy, celda_tamanio, celda_tamanio );
+        rect(posx, posy, celda_tamanio, celda_tamanio);
+
+  }else if(tablero[x][y] == 1 && vecinos ==2){
+        fill(10, 0, 200);
+        stroke(0);
+        rect(posx, posy, celda_tamanio, celda_tamanio);
+
+    }else if(tablero[x][y] == 1 && vecinos ==3){
+        fill(80, 100, 0);
+        stroke(0);
+        rect(posx, posy, celda_tamanio, celda_tamanio);
         }
-      
+
       }
     }
   }
@@ -82,7 +115,7 @@ function pintatablero(cols,ren){
 function creaTablero(cols, ren){
    let tab = new Array(cols);
    for(let i  = 0; i < tab.length; i = i + 1){
-     tab[i] = new Array(ren);    
+     tab[i] = new Array(ren);
   }
   return tab;
-} 
+}
